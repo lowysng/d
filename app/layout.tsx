a9d25110f255385e1f96c2f68446865a980e876d
lowysng <lowysng@gmail.com>
2023-10-02 23:31:45 +0800

    update website title

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,12 +4,11 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Toaster } from "@/components/ui/toaster";
-import { Button } from "@/components/ui/button";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-    title: "monyet.io",
+    title: "study",
 };
 
 export default function RootLayout({
