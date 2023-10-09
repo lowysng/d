49471b0f90f20627d97d41d92757dfb6fa12ed12
lowysng <lowysng@gmail.com>
2023-10-09 11:42:54 +0800

    add error handling for /api/push-notification'

--- app/api/push-notification/route.ts
+++ app/api/push-notification/route.ts
@@ -4,17 +4,21 @@ import { NextRequest } from "next/server";
 export async function POST(req: NextRequest) {
     const { message } = await req.json();
 
-    fetch("https://api.pushover.net/1/messages.json", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-            token: process.env.PUSHOVER_API_KEY,
-            user: process.env.PUSHOVER_USER_KEY,
-            message,
-        }),
-    });
+    try {
+        await fetch("https://api.pushover.net/1/messages.json", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                token: process.env.PUSHOVER_API_KEY,
+                user: process.env.PUSHOVER_USER_KEY,
+                message,
+            }),
+        });
+    } catch (e) {
+        console.log("error", e);
+    }
 
     return Response.json({ data: "ok" });
 }
